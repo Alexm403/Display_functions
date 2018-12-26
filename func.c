369be0b235c25062b7ad50ddb7a1246fe5d1846f
Alexm403 <alexm403@gmail.com>
2018-12-26 11:08:07 +0200

    Create func.c

---
+++ func.c
@@ -0,0 +1,7 @@
+char sRow=8,sCol=132;
+//разрешение экрана (высота 1 байт = 8 пикселей, тоесть если дисплей 64 пикслея, задаем 8), по горизонтали оставляем как есть
+
+unsigned char Buffer[sRow][sCol];  //unsigned char Buffer[sRow][sCol]; 
+
+
+
